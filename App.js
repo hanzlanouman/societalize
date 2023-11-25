35c90fa91f624de19510b629b1560ad09b0d25e0
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2023-11-25 11:19:56 +0500

    Changed Structure

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './screens/Login';
-import Bla from './components/Bla';
 
 export default function App() {
   return (
